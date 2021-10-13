8fea14b43e7a8491f736b8a85a6b56c3230ee6b7
Matheus Castello <matheus@castello.eng.br>
2021-10-13 17:43:48 -0300

    Blink: Add Platforms static class and Nezha support
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ Blink/Platforms.cs
@@ -0,0 +1,11 @@
+
+namespace DotnetNFPosix {
+    public class Platforms {
+        public const string WSL = "wsl";
+        public const string PI_PICO = "pi-pico";
+        public const string PI_ZERO = "pi-zero";
+        public const string ESP32_C3 = "esp32c3";
+        public const string STARFIVE_JH7100 = "jh7100";
+        public const string NEZHA_ALLWINNER_D1 = "nezha";    
+    }
+}
--- Blink/Program.cs
+++ Blink/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Threading;
 using System.Device.Gpio;
 using nanoFramework.Runtime.Native;
+using DotnetNFPosix;
 
 Debug.WriteLine($"Running nanoFramework on {SystemInfo.OEMString}");
 
@@ -14,22 +15,29 @@ try
     GpioPin led;
 
     switch (SystemInfo.TargetName) {
-        case "beagle-v":
+        case Platforms.STARFIVE_JH7100:
             // pin 16 in the header is the gpio21
             ledPinNumber = 21;
         break;
-        case "pi-zero":
+        case Platforms.PI_ZERO:
             // pin 16 in the header is the gpio23
             ledPinNumber = 23;
         break;
-        case "pi-pico":
+        case Platforms.PI_PICO:
             // onboard LED
             ledPinNumber = 25;
         break;
-        case "esp32c3":
+        case Platforms.ESP32_C3:
             // pin 6 from DevKit is gpio8
-            ledPinNumber = 8;
+            ledPinNumber = 7;
         break;
+        case Platforms.NEZHA_ALLWINNER_D1:
+            // pin 16 in the header is the ioexpander pp1
+            ledPinNumber = 1;
+        break;
+        case Platforms.WSL:
+            Debug.WriteLine($"The platform {SystemInfo.Platform}:{SystemInfo.TargetName} does not have GPIO support!");
+            return;
         default:
             throw new Exception($"Your target [{SystemInfo.TargetName}] does not support GPIOs");
     }
